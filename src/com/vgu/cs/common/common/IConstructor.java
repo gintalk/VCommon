fb9e807be081b350443e0b2ee243933a0fe8c6e5
namnh16 <hnam9430@gmail.com>
2021-02-25 09:05:52 +0700

    First move

---
+++ src/com/vgu/cs/common/common/IConstructor.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2012-2016 by Zalo Group.
+ * All Rights Reserved.
+ */
+package com.vgu.cs.common.common;
+
+/**
+ *
+ * @author namnh16
+ */
+public interface IConstructor<T> {
+
+    T constructor();
+}
